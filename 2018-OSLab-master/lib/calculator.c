b6bb6e80bcfdc5019e39ecf4bce6b79721ef63da
debuggingggg <43994360+duiyoune@users.noreply.github.com>
2020-08-19 15:50:30 +0800

    czj0.4

--- 2018-OSLab-master/lib/calculator.c
+++ 2018-OSLab-master/lib/calculator.c
@@ -1,11 +1,3 @@
-/*************************************************************************//**
- *****************************************************************************
- * @file   mkdir.c
- * @brief  mkdir
- * @author zwz
- * @date   2018
- *****************************************************************************
- *****************************************************************************/
 
 #include "type.h"
 #include "stdio.h"
@@ -19,153 +11,148 @@
 #include "global.h"
 #include "proto.h"
 
-#define MAX_SIZE 1024                       /*数组长度*/
+typedef struct node {
+    double date;
+    char opt;
+    struct node* pre;
+}* pnode, node;
 
+typedef struct stack {
+    pnode bottom;   //指向底部
+    pnode top;  //指向头部
+}* pstack, stack;
 
-int insert_operand(int *operand , int * top_num ,int num)           /*数据压入数据栈*/
-{
-    (*top_num) ++;
-    operand[*top_num] = num;                    /*保存数据*/
-    
-    return 0;                           /*正常退出*/
+pstack create_stack(void) {
+    pstack s = (pstack)malloc(sizeof(stack));
+    s->bottom = s->top = (pnode)malloc(sizeof(node));
+    return s;
 }
 
-
-int insert_oper (char * oper , int *top_oper , char ch)             /*操作符压入符号栈*/
-{
-    (*top_oper)++;
-    oper[*top_oper] = ch;                       /*保存操作符*/
-    return 0;                           /*正常退出*/
+void push(pstack s, char date) {
+    pnode t = (pnode)malloc(sizeof(node));
+    t->pre = s->top;
+    t->date = date;
+    s->top = t;
 }
-     
-int compare(char *oper , int *top_oper , char ch)                   /*比较操作服优先级*/
-{
-    if((oper[*top_oper] == '-' || oper[*top_oper] == '+')           /*判断当前优先级是否比栈顶操作符优先级高*/
-    && (ch == '*' || ch == '/'))
-    {
-        return 0;                      /*操作符压入栈*/
+
+void pop(pstack s) {
+    if (s->top == s->bottom) {
+        printf("不能弹出，此时栈为空\n");
+        return;
     }
-    else if(*top_oper == -1 || ch == '('|| (oper[*top_oper] == '(' && ch != ')'))  /*判断操作符栈是否为空；栈顶操作   符是否为'('*/
-    {
-        return 0;                       /*操作符压入栈*/
+    pnode t = s->top;
+    s->top = s->top->pre;
+    free(t);
+}
+
+int empty(pstack s) {
+    if (s->top == s->bottom) {
+        return 1;
     }
-    else if (oper[*top_oper] =='(' && ch == ')' )       /*判断括号内的表达式是否计算完毕*/
-    {
-        (*top_oper)--;
-        return 1;                       /*对（）进行处理*/
-    }
-    else
-    {
-        return -1;                                          /*进行操作符的运算*/
+    else {
+        return 0;
     }
 }
 
-
-int deal_date(int *operand ,char *oper ,int *top_num, int *top_oper)    /*进行数据运算*/
-{
-    int num_1 = operand[*top_num];              /*取出数据栈中两个数据*/
-    int num_2 = operand[*top_num - 1];
-    
-    int value = 0;
-    
-    if(oper[*top_oper] == '+')                  /*加法操作*/
-    {
-        value = num_1 + num_2;
-    }
-    
-    else if(oper[*top_oper] == '-')             /*减法操作*/
-    {
-        value = num_2 - num_1;
-    }
-    
-    else if(oper[*top_oper] == '*')             /*乘法操作*/
-    {
-        value = num_2 * num_1;
-    }
-    
-    else if(oper[*top_oper] == '/')             /*除法操作*/
-    {
-        value = num_2 / num_1;
-    }
-    
-    (*top_num) --;                              /*将数据栈顶下移一位*/
-    operand[*top_num] = value;                  /*将得到的值压入数据栈*/
-    (*top_oper) --;                             /*将操作符栈顶下移一位*/
+int top(pstack s) {
+    return s->top->date;
 }
 
-PUBLIC int Calculator()
-{
-    int operand[MAX_SIZE] = {0};                /*数据栈，初始化*/
-    int  top_num = -1;
-    
-    char oper[MAX_SIZE] = {0};                  /*操作符栈，初始化*/
-    int top_oper = -1;
-    char str[100];               /*获取表达式(不带＝)*/
-    
-    printf("Please enter the expression!\n");
-    char tty_name[] = "/dev_tty0";
+void push_num(pstack s, int date) {
+    pnode t = (pnode)malloc(sizeof(node));
+    t->pre = s->top;
+    t->date = date;
+    s->top = t;
+}
 
-    char rdbuf[256];
-    int fd_stdin  = open(tty_name, O_RDWR);
-
-
-    int r = read(fd_stdin, rdbuf, 512);
-        rdbuf[r] = 0;
-
-    // 解析命令
-    int pos = 0;
-    while (rdbuf[pos] != ' ' && rdbuf[pos] != 0)  // 读取指令
-    {
-        str[pos] = rdbuf[pos];
-        pos++;
-    }
-    str[pos] = 0;
-    printf("%s \n",str);
-    char* temp;
-    char dest[MAX_SIZE];
-    
-    int num = 0;
-    int i = 0;
-    int ind=0;
-    while(str[ind] != '\0')
-    {
-        temp = dest;
-        while(str[ind] >= '0' && str[ind] <= '9')           /*判断是否是数据*/
-        {
-            *temp = str[ind];
-            ind ++;
-            temp ++;
-        }                               /*遇到符号退出*/
-       
-        if(str[ind] != '(' && *(temp - 1) != '\0')      /*判断符号是否为'('*/
-        {
-            *temp = '\0';
-            atoi(dest, &num);               /*将字符串转为数字*/
-            insert_operand(operand, &top_num,num);      /*将数据压入数据栈*/
+PUBLIC int Calculator(char *str) {
+    pstack s_opt = create_stack();
+    pstack s_num = create_stack();
+    char suffix[100];
+    int opt[100];
+    memset(opt, 0, sizeof(opt));
+    opt['+'] = opt['-'] = 1;
+    opt['*'] = opt['/'] = 2;
+    opt['('] = opt[')'] = 0;
+    int len = strlen(str), p = 0, flag = 1;
+    //printf("%d\n",len);
+    for (int i = 0; i < len; i++) {
+        if (str[i] <= '9' && str[i] >= '0') {
+            suffix[p++] = str[i];
+            if (i+1 < len && str[i+1] <= '9' && str[i+1] >= '0');
+            else    suffix[p++] = ' ';
+            flag = 0;
+            continue;
         }
-
-        while(1)
-        {
-            i = compare(oper,&top_oper,str[ind]);      /*判断操作符优先级*/
-            if(i == 0)
-            {
-                insert_oper(oper,&top_oper,str[ind]);   /*压入操作符*/
-                break;
+        else {
+            flag++;
+        }
+        if (str[i] == '(') {
+            push(s_opt, str[i]);
+        }
+        else if (str[i] == ')') {
+            while(top(s_opt) != '(') {
+                //printf("%c\n", top(s_opt));
+                suffix[p++] = top(s_opt);
+                pop(s_opt);
             }
-   
-            else if(i == 1)                         /*判断括号内的表达式是否结束*/
-            {
-                ind ++;
+            pop(s_opt);
+            //printf("1");
+        }
+        else if (empty(s_opt) && str[i] != ' ') {
+            push(s_opt, str[i]);
+        }
+        else {
+            while(!empty(s_opt) && opt[str[i]] <= opt[top(s_opt)]) {
+                suffix[p++] = top(s_opt);
+                pop(s_opt);
             }
-   
-            else if(i == -1)                        /*进行数据处理*/
-            {
-                deal_date(operand,oper,&top_num,&top_oper);
+            push(s_opt, str[i]);
+        }
+        if (flag >= 1) {
+            //    suffix[p++] = ' ';
+        }
+    }
+    while(!empty(s_opt)) {
+        suffix[p++] = top(s_opt);
+        pop(s_opt);
+    }
+    for (int i = 0; i < p-1; i++) {
+        printf("%c",suffix[i]);
+    }
+    printf("\n");
+    pstack number_stack = create_stack();
+    flag = 0;
+    double sum = 0, ans = 0;
+    for (int i = 0; i < p; i++) {
+        if (suffix[i] <= '9' && suffix[i] >= '0') {
+            sum = sum*10 + (suffix[i]-'0');
+            flag = 1;
+        }
+        else {
+            if (flag == 1) {
+                push(number_stack, sum);
+                sum = 0;
+                flag = 0;
+            }
+            if (suffix[i] == '+' || suffix[i] == '-' || suffix[i] == '*' || suffix[i] == '/') {
+                double t1 = number_stack->top->date;
+                pop(number_stack);
+                if (suffix[i] == '+') {
+                    number_stack->top->date += t1;
+                }
+                else if (suffix[i] == '-') {
+                    number_stack->top->date -= t1;
+                }
+                else if (suffix[i] == '*') {
+                    number_stack->top->date *= t1;
+                }
+                else if (suffix[i] == '/') {
+                    number_stack->top->date /= t1;
+                }
             }
         }
-        ind ++;                 /*指向表达式下一个字符*/
     }
-    
-    printf("num = %d\n",operand[0]);        /*输出结果*/
-    return 0;                       /*正常退出*/
+    printf("%lf",number_stack->top->date);
+    return 0;
 }
